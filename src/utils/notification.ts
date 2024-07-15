33f16fc431c6c89c868752fe7d22aa948fc817a7
OHS <ohs6006@gmail.com>
2024-07-16 01:06:03 +0900

    메세지 수신 테스트 2

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,18 +1,3 @@
-export const registerServiceWorker = () => {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then((registration) => {
-          console.log("Service Worker 등록 성공:", registration);
-        })
-        .catch((error) => {
-          console.log("Service Worker 등록 실패:", error);
-        });
-    });
-  }
-};
-
 export const requestNotificationPermission = () => {
   if ("Notification" in window) {
     Notification.requestPermission().then((permission) => {
