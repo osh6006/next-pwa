7e0fe7c709a7f930e884bc4ea507c5cffdb58cb3
OHS <ohs6006@gmail.com>
2024-07-15 20:46:09 +0900

    코드 복사 버튼

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [showToken, setShowToken] = useState("");
+  const [copySuccess, setCopySuccess] = useState(false);
 
   // 푸시 알림 테스트
   const handlePush = () => {
@@ -45,43 +46,73 @@ export default function Home() {
       });
   };
 
+  const copyToClipboard = async () => {
+    if (!showToken) return;
+
+    try {
+      await navigator.clipboard.writeText(showToken);
+      setCopySuccess(true);
+
+      // 3초 후에 상태 초기화
+      setTimeout(() => {
+        setCopySuccess(false);
+      }, 800);
+    } catch (err) {
+      setCopySuccess(false);
+
+      // 3초 후에 상태 초기화
+      setTimeout(() => {
+        setCopySuccess(false);
+      }, 800);
+    }
+  };
+
   return (
-    <main className="flex h-dvh w-dvw mx-auto border-x flex-col items-center justify-center p-24">
-      <div className="flex-col w-full relative z-[-1] flex place-items-center before:absolute before:h-[100px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <h1 className="mt-20 text-xl text-center break-keep font-bold">
-          PWA Test App 입니다.{" "}
-        </h1>
-      </div>
+    <main className="flex flex-col max-w-[400px] mx-auto w-screen h-screen overflow-hidden items-center justify-center p-10">
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+      <h1 className="mt-4 text-xl text-center break-keep font-bold">
+        PWA Test App 입니다.{" "}
+      </h1>
       <button
-        className="mt-10 border p-3 rounded-md cursor-pointer select-none z-40"
+        className="mt-4 border p-3 rounded-md cursor-pointer select-none z-40"
         onClick={handlePush}
       >
         푸시 알림 보내기
       </button>
       <button
-        className="mt-10 border p-3 rounded-md cursor-pointer select-none z-40"
+        className="mt-4 border p-3 rounded-md cursor-pointer select-none z-40"
         onClick={handlePermission}
       >
         알람 허용 버튼
       </button>
 
       <button
-        className="mt-10 border p-3 rounded-md cursor-pointer select-none z-40"
+        className="mt-4 border p-3 rounded-md cursor-pointer select-none z-40"
         onClick={handleGetToken}
       >
         등록 토큰 받기
       </button>
-      <div className="break-keep text-center">
-        <p className="mt-4">아래의 개발자에게 토큰을 보내주세요</p>
-        <p className="text-xs mt-4">{!showToken ? "토큰 없음" : showToken}</p>
+      <div className="text-center">
+        <p className="mt-4">아래의 토큰을 개발자에게 보내주세요</p>
+        <p className="text-xs mt-4 break-words">
+          {!showToken ? "토큰 없음" : showToken}
+        </p>
+        {!showToken ? null : (
+          <button
+            onClick={copyToClipboard}
+            className="bg-green-900 p-2 rounded-md mt-2"
+            disabled={copySuccess}
+          >
+            {copySuccess ? "복사 성공!" : "코드 복사"}
+          </button>
+        )}
       </div>
     </main>
   );
