6ec751e3fc936f8fabd3d2acea80caad0a5069b1
OHS <ohs6006@gmail.com>
2024-07-15 21:09:22 +0900

    디자인 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,13 @@
 import { app, setTokenHandler } from "@/lib/firebase";
 import { sendPushNotification } from "@/utils/notification";
 import { getMessaging, getToken } from "firebase/messaging";
+import {
+  BellPlusIcon,
+  BellRingIcon,
+  ClipboardCheckIcon,
+  ClipboardIcon,
+  ScanTextIcon,
+} from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -80,36 +87,52 @@ export default function Home() {
       <h1 className="mt-4 text-xl text-center break-keep font-bold">
         PWA Test App 입니다.{" "}
       </h1>
-      <button
-        className="mt-4 border p-3 rounded-md cursor-pointer select-none z-40"
-        onClick={handlePush}
-      >
-        푸시 알림 보내기
-      </button>
-      <button
-        className="mt-4 border p-3 rounded-md cursor-pointer select-none z-40"
-        onClick={handlePermission}
-      >
-        알람 허용 버튼
-      </button>
 
-      <button
-        className="mt-4 border p-3 rounded-md cursor-pointer select-none z-40"
-        onClick={handleGetToken}
-      >
-        등록 토큰 받기
-      </button>
+      <div className="space-y-5 py-8 flex flex-col">
+        <button
+          onClick={handlePush}
+          type="button"
+          className="text-gray-900 gap-x-2 justify-center bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-semibold rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 "
+        >
+          <BellRingIcon size={18} />
+          푸시 알림 보내기
+        </button>
+        <button
+          onClick={handlePermission}
+          type="button"
+          className="text-gray-900 gap-x-2 justify-center bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-semibold rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
+        >
+          <BellPlusIcon size={18} />
+          알람 허용 버튼
+        </button>
+        <button
+          onClick={handleGetToken}
+          type="button"
+          className="text-gray-900 gap-x-2 justify-center bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-semibold rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
+        >
+          <ScanTextIcon size={18} />
+          등록 토큰 받기
+        </button>
+      </div>
+
       <div className="text-center">
         <p className="mt-4">아래의 토큰을 개발자에게 보내주세요</p>
         <p className="text-xs mt-4 break-words">
-          {!showToken ? "토큰 없음" : showToken}
+          {!showToken
+            ? "토큰이 없습니다 토큰 받기 버튼을 클릭하세요"
+            : showToken}
         </p>
         {!showToken ? null : (
           <button
             onClick={copyToClipboard}
-            className="bg-green-900 p-2 rounded-md mt-2"
-            disabled={copySuccess}
+            type="button"
+            className="text-gray-900 my-2 gap-x-2 justify-center bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-semibold rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-green-800 dark:border-green-600 dark:text-white dark:hover:bg-green-700"
           >
+            {copySuccess ? (
+              <ClipboardCheckIcon size={18} />
+            ) : (
+              <ClipboardIcon size={18} />
+            )}
             {copySuccess ? "복사 성공!" : "코드 복사"}
           </button>
         )}
